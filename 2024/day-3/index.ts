411d2d07f2e78a010159689f84fac41f453c8737
bnonni <github@nonni.dev>
2024-12-03 20:19:26 -0500

    day-3

---
+++ 2024/day-3/index.ts
@@ -0,0 +1,14 @@
+import data from './data.js'
+
+const corrupted = data.trim();
+const pattern = /mul\([0-9]{1,3},[0-9]{1,3}\)/g;
+const instructions = corrupted.match(pattern) ?? [];
+let sum = 0;
+instructions
+    .map(instruction => instruction
+        .replace('mul(', '')
+        .replace(')', '')
+        .split(',')
+        .map(Number)
+        .reduce((a, b) => sum += (a * b)));
+console.log(`Sum of muls: ${sum}`);
